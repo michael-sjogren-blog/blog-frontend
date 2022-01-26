9ad6fa18ea383250560ced18e7914a368913977c
michael <carl.michael.sjogren@gmail.com>
2022-01-26 22:57:22 +0100

    form error component

---
+++ src/lib/FormError.svelte
@@ -0,0 +1,20 @@
+<small class="error">
+    <slot/>
+</small>
+
+<style>
+    :global(.error) {
+        margin: 1em auto;
+        display: block;
+        color: var(--danger);
+    }
+
+    :global(.error::before){
+        content: '*';
+        font-size: 1rem;
+        font-weight: bold;
+        margin-right: .5em;
+        width: 30px;
+        height: 30px;
+    }
+</style>
