c93393558acac03ce55e10395feebdc064cfa8bd
michael <carl.michael.sjogren@gmail.com>
2022-01-26 22:26:05 +0100

    fetching posts on mount and saving to store

--- src/routes/blog.svelte
+++ src/routes/blog.svelte
@@ -1,71 +1,65 @@
 <script>
-import { onMount } from "svelte";
-import CreatePostForm from "$lib/CreatePostForm.svelte";
-import { deleteOne, endpoints, getAll } from "../services/Api.js";
+    import CreatePostForm from "$lib/CreatePostForm.svelte";
+    import { deleteOne, endpoints, getAll } from "../services/Api.js";
+    import { postsStore } from "../store.js";
+    import { onMount } from "svelte";
 
-
-    let posts = [];
-    onMount(() => {
-        getAll(endpoints.posts).then(
-            data => posts = data
-        )
-    })
-
+    onMount(() =>
+        getAll(endpoints.posts).then((data) => {
+            $postsStore = data;
+            console.log($postsStore)
+        })
+    );
     const onDeletePost = (id) => {
-        deleteOne(endpoints.posts,id)
-    }
-
+        deleteOne(endpoints.posts, id).then(() => {
+            const index = $postsStore.findIndex((e) => e.id === id);
+            if (index > -1) {
+                let newPosts = [...$postsStore];
+                newPosts.splice(index, 1);
+                $postsStore = [...newPosts];
+            }
+        });
+    };
 </script>
-<h1>
-    Heading 1
-</h1>
 
-<h2>
-    Heading 2
-</h2>
+<h1>Heading 1</h1>
 
-<h3>
-    Heading 3
-</h3>
-<h4>
-    Heading 4
-</h4>
+<h2>Heading 2</h2>
 
-<h5>
-    Heading 5
-</h5>
-<br>
-<hr>
-<CreatePostForm></CreatePostForm>
+<h3>Heading 3</h3>
+<h4>Heading 4</h4>
+
+<h5>Heading 5</h5>
+<br />
+<hr />
+<CreatePostForm />
 <section>
-    <hr>
+    <hr />
     <h2>Blog Posts</h2>
-    {#if posts}
+    {#if $postsStore === undefined}
         <p>No Posts exists.</p>
+    {:else}
+        {#each $postsStore as { id, title, content } (id)}
+            <article class="post">
+                <header class="post-header">
+                    <h5 class="title">
+                        <a href={`/blog/posts/${id}`}>
+                            {title}
+                        </a>
+                    </h5>
+                    <span>
+                        <button on:click={onDeletePost(id)} class="btn delete">
+                            x
+                        </button>
+                    </span>
+                </header>
+                <p class="content-snapshot">{@html content}</p>
+            </article>
+        {/each}
     {/if}
-    {#each posts as {id,title, content} (id)}
-        <article class="post">
-            <header class="post-header">
-                <h5 class="title">
-                    <a href={`/blog/posts/${id}`}>
-                        {title}
-                    </a>
-                </h5>
-                <span>
-                    <button on:click="{onDeletePost(id)}" class="btn delete">
-                        x
-                    </button>
-
-                </span>
-            </header>
-            <p class="content-snapshot">{@html content}</p>
-        </article>
-    {/each}
 </section>
 
-
 <style>
-
     .delete {
         color: var(--pop);
     }
@@ -84,12 +78,12 @@ import { deleteOne, endpoints, getAll } from "../services/Api.js";
         background-color: var(--white);
         max-width: 900px;
         border-radius: 1rem;
-        box-shadow: .1em .1em .1em #00000028;
+        box-shadow: 0.1em 0.1em 0.1em #00000028;
         padding: 2em;
-        margin: 2em 0;        
+        margin: 2em 0;
     }
 
-    .content-snapshot{
+    .content-snapshot {
         text-align: justify;
         line-height: 1.5rem;
         max-height: 200px;
